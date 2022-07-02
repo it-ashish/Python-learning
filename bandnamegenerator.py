1501eae87293264b92c02a2b556d471bea32cdd5
ashish <ashish58ashu@gmail.com>
2022-07-02 13:19:16 +0530

    final

---
+++ bandnamegenerator.py
@@ -0,0 +1,3 @@
+city = input("what's name of the city you grew up in?")
+name = input("what is your name")
+print(city, name)
--- input.py
+++ input.py
@@ -1,3 +1,4 @@
-
-a = input("enter any character : ")
-print(len(a))
+a = input("a: ")
+b = input("b: ")
+a,b = b,a
+print(a, b)
